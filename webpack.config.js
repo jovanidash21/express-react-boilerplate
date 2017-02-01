4da5d3d82e24247e78900f3fe40f56fe905e1220
jovanidash21 <jwardash21@gmail.com>
2017-02-02 00:01:38 +0800

    Add Webpack Config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: path.join(__dirname, '/public/build'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['latest', 'react']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
